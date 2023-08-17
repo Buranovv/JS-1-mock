9f39d0fedc986c56063128beb7fbe1707153459e
Buranovv <zahiriddinburanov007@gmail.com>
2023-08-17 21:46:48 +0500

    3-exercise done

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,16 +1,29 @@
-/* Exercise №2*/
-const num = 1611;
-const toStr = num.toString();
-const toArr = toStr.split("");
+// /* Exercise №1*/
 
-function namd(number) {
-  const reversed = toArr.reverse();
-  const toStr2 = reversed.join("");
-  if (toStr2 == toStr) {
-    console.log(true);
-  } else {
-    console.log(false);
-  }
+
+
+// /* Exercise №2*/
+// const num = 165651;
+// const toStr = num.toString();
+// const toArr = toStr.split("");
+
+// function namd(number) {
+//   const reversed = toArr.reverse();
+//   const toStr2 = reversed.join("");
+//   if (toStr2 == toStr) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+
+// namd(toArr);
+
+/* Exercise №3*/
+const year = 209;
+
+if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) {
+  console.log(`${year} is Leap year`);
+} else {
+  console.log(`${year} is not Leap year`);
 }
-
-namd(toArr);
