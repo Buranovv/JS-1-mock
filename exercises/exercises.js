cf63bfd8484ca8fd06f529e1d0f5556c105d02e4
Buranovv <zahiriddinburanov007@gmail.com>
2023-08-17 21:58:48 +0500

    1-exercise done

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,6 +1,29 @@
 // /* Exercise №1*/
+const arr = [2, 5, -4, 6, -5];
 
+function sorter() {
+  //Counter
+  const arrFilterCount = arr.filter((element) => {
+    return element >= 0;
+  });
+  let count = 0;
+  arrFilterCount.forEach((element) => {
+    count = count + 1;
+  });
+  console.log(count);
 
+  //Sum counter
+  const arrFilterSum = arr.filter((element) => {
+    return element < 0;
+  });
+  let sum = 0;
+
+  arrFilterSum.forEach((element) => {
+    sum = sum + element;
+  });
+  console.log(sum);
+}
+sorter();
 
 // /* Exercise №2*/
 // const num = 165651;
@@ -19,11 +42,11 @@
 
 // namd(toArr);
 
-/* Exercise №3*/
-const year = 209;
+// /* Exercise №3*/
+// const year = 209;
 
-if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) {
-  console.log(`${year} is Leap year`);
-} else {
-  console.log(`${year} is not Leap year`);
-}
+// if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) {
+//   console.log(`${year} is Leap year`);
+// } else {
+//   console.log(`${year} is not Leap year`);
+// }
