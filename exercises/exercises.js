8d53ecca271d563817489975ad212275be465a1d
Buranovv <zahiriddinburanov007@gmail.com>
2023-08-18 10:32:54 +0500

    4-exercise done

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,31 +1,31 @@
-// /* Exercise №1*/
-const arr = [2, 5, -4, 6, -5];
+/* Exercise №1 */
+// const arr = [2, 5, -4, 6, -5];
 
-function sorter() {
-  //Counter
-  const arrFilterCount = arr.filter((element) => {
-    return element >= 0;
-  });
-  let count = 0;
-  arrFilterCount.forEach((element) => {
-    count = count + 1;
-  });
-  console.log(count);
+// function sorter() {
+//   //Counter
+//   const arrFilterCount = arr.filter((element) => {
+//     return element >= 0;
+//   });
+//   let count = 0;
+//   arrFilterCount.forEach((element) => {
+//     count = count + 1;
+//   });
+//   console.log(count);
 
-  //Sum counter
-  const arrFilterSum = arr.filter((element) => {
-    return element < 0;
-  });
-  let sum = 0;
+//   //Sum counter
+//   const arrFilterSum = arr.filter((element) => {
+//     return element < 0;
+//   });
+//   let sum = 0;
 
-  arrFilterSum.forEach((element) => {
-    sum = sum + element;
-  });
-  console.log(sum);
-}
-sorter();
+//   arrFilterSum.forEach((element) => {
+//     sum = sum + element;
+//   });
+//   console.log(sum);
+// }
+// sorter();
 
-/* Exercise №2*/
+/* Exercise №2 */
 // const num = 165651;
 // const toStr = num.toString();
 // const toArr = toStr.split("");
@@ -42,15 +42,39 @@ sorter();
 
 // namd(toArr);
 
-/* Exercise №3*/
-const year = 209;
+/* Exercise №3 */
+// const year = 209;
 
-function findLeap() {
-  if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) {
-    console.log(`${year} is Leap year`);
-  } else {
-    console.log(`${year} is not Leap year`);
-  }
+// function findLeap() {
+//   if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) {
+//     console.log(`${year} is Leap year`);
+//   } else {
+//     console.log(`${year} is not Leap year`);
+//   }
+// }
+
+// findLeap();
+
+/* Exercise №4 1-way */
+// const num = [1, -4, 4, -2, 4, -5];
+
+// function findNegative(array) {
+//   const newArr = [];
+//   num.forEach((element) => {
+//     if (element < 0) {
+//       newArr.push(element);
+//     }
+//   });
+//   console.log(newArr);
+// }
+
+// findNegative(num);
+
+/* Exercise №4 2-way */
+const num = [1, -4, 4, -2, 4, -5];
+
+function negatives(array) {
+  console.log(array.filter((element) => element < 0));
 }
 
-findLeap();
+negatives(num);
